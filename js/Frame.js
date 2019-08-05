20060f5d5068b6817cce62e05de6b38e9af9a0a8
Jacob <jacobphillipashe@googlemail.com>
2019-08-05 09:07:13 +0100

    scaling for frame

--- js/Frame.js
+++ js/Frame.js
@@ -17,7 +17,8 @@ export default class Frame extends Component {
     super(props);
     this.state = {
       position: [0, 0, -0.1],
-      scale: 1
+      scale: 1,
+      prevScale: 0
     };
   }
 
@@ -27,9 +28,9 @@ export default class Frame extends Component {
       <View>
         <ViroImage
           scale={[
-            (1 * this.props.image.width * this.state.scale) /
+            (0.5 * this.props.image.width * this.state.scale) /
               this.props.image.height,
-            1 * this.state.scale,
+            0.5 * this.state.scale,
             1
           ]}
           position={this.state.position}
@@ -61,7 +62,7 @@ export default class Frame extends Component {
           -0.1
         ]
       }),
-      1,
+      5,
       false
     );
   };
